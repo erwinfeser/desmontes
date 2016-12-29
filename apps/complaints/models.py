32830b815574d4fe583a4f2163e9717fdb7c5881
Erwin Feser <erwin.feser@gmail.com>
2016-12-28 22:59:48 -0300

    str

--- apps/complaints/models.py
+++ apps/complaints/models.py
@@ -13,6 +13,9 @@ class AbstractCreatedUpdated(models.Model):
 class Profile(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='profile')
 
+    def __str__(self):
+        return self.user.email
+
 
 class Complaint(AbstractCreatedUpdated):
     profile = models.ForeignKey('Profile', related_name='complaints')
