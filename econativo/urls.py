e5fb1fac23f967a9dc53f798d0738246ddc88ccd
Erwin Feser <erwin@feser.net.ar>
2017-04-10 22:58:55 -0300

    Working with Telegram backend

--- econativo/urls.py
+++ econativo/urls.py
@@ -20,7 +20,6 @@ from apps.layers.api import routers
 
 urlpatterns = [
     url(r'^api/v1/', include(routers, 'v1')),
-    url(r'^raster/', include('raster.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(pattern_name='admin:index', permanent=True)),
 ]
